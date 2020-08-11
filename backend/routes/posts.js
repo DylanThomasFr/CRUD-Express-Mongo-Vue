503862491d589401c364d67a047e15456f27889b
Dylan.THOMAS <dylan.thomas@etu.univ-lyon1.fr>
2020-08-11 11:18:04 +0200

    Add readonly check for post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -43,32 +43,42 @@ router.get('/:postId', verify, async (request, response) => {
         const post = await Post.findById(request.params.postId)
         response.json(post)
     } catch (err) {
-        response.json({ message: err })
+        response.status(400).json({ message: err })
     }
 })
 
 //DELETE A POST
 router.delete('/:postId', verify, async (request, response) => {
-    try {
-        const removedPost = await Post.remove({ _id: request.params.postId })
-        response.json(removedPost)
-    } catch (err) {
-        response.json({ message: err })
+    const user = await User.findOne({ _id: request.user._id })
+    if (!user.readonly) {
+        try {
+            const removedPost = await Post.remove({ _id: request.params.postId })
+            response.status(200).json({message: 'Post removed successfully'})
+        } catch (err) {
+            response.status(400).json({ message: err })
+        }
+    }else{
+        response.status(401).send({message:'You can\'t delete a post'})
     }
 })
 
 //UPDATE A POST
 router.put('/:postId', verify, async (request, response) => {
-    try {
-        const updatedPost = await Post.updateOne({ _id: request.params.postId }, {
-            $set: {
-                title: request.body.title,
-                content: request.body.content
-            }
-        })
-        response.json(updatedPost)
-    } catch (err) {
-        response.json({ message: err })
+    const user = await User.findOne({ _id: request.user._id })
+    if (!user.readonly) {
+        try {
+            const updatedPost = await Post.updateOne({ _id: request.params.postId }, {
+                $set: {
+                    title: request.body.title,
+                    content: request.body.content
+                }
+            })
+            response.status(200).json({message: 'Post removed successfully'})
+        } catch (err) {
+            response.status(400).json({ message: err })
+        }
+    }else{
+        response.status(401).send({message:'You can\'t update a post'})
     }
 })
 
